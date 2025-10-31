ab56faa07676606b6ca27529c20ca23f89a34163
Данил Невзоров <danilnevzorov@MacBook-Air-Danil.local>
2025-10-31 10:52:37 +0300

    add padding

--- Unit 1/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ Unit 1/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +35,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Cyan) {
         Text(
             text = "Hi, my name is $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
