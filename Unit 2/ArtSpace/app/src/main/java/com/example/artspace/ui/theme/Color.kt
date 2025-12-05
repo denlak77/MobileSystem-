2cb8afc3c4fb876dfb61c9bfc7d924dddad48a73
Данил Невзоров <danilnevzorov@MacBook-Air-Danil.local>
2025-12-05 15:38:28 +0300

    Add ArtSpace project

---
+++ Unit 2/ArtSpace/app/src/main/java/com/example/artspace/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.example.artspace.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ButtonColor = Color(0xFF1976D2)
+
+// Light theme colors
+val ArtSpaceBackgroundLight = Color(0xFF424242)
+val ArtSpaceCardBackgroundLight = Color.White
+val ArtSpaceImageBackgroundLight = Color(0xFFF5F5F5)
+val ArtSpaceOverlayBackgroundLight = Color(0x80000000)
+val ArtSpaceTextPrimaryLight = Color.White
+val ArtSpaceTextSecondaryLight = Color(0xFFBDBDBD)
+
+// Dark theme colors
+val ArtSpaceBackgroundDark = Color(0xFF1C1C1C)
+val ArtSpaceCardBackgroundDark = Color(0xFF2C2C2C)
+val ArtSpaceImageBackgroundDark = Color(0xFF3C3C3C)
+val ArtSpaceOverlayBackgroundDark = Color(0x90000000)
+val ArtSpaceTextPrimaryDark = Color(0xFFE0E0E0)
+val ArtSpaceTextSecondaryDark = Color(0xFF9E9E9E)
