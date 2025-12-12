7bd4589ddbeb59f8b2c03c4e820b133d565ecb53
Данил Невзоров <danilnevzorov@MacBook-Air-Danil.local>
2025-12-12 11:10:41 +0300

    add class Quiz

---
+++ Unit 3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
@@ -0,0 +1,75 @@
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
+fun main() {
+    val quiz = Quiz()
+    quiz.printQuiz()
+
+    Quiz().apply {
+        printQuiz()
+    }
+
+    Quiz.printProgressBar();
+}
