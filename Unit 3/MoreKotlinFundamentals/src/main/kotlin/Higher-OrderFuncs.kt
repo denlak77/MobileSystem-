d364272bf3a91284f6511a2b49480c37f4eb0b8b
Данил Невзоров <danilnevzorov@MacBook-Air-Danil.local>
2025-12-12 11:25:04 +0300

    Add work with Higher-OrderFuncs

---
+++ Unit 3/MoreKotlinFundamentals/src/main/kotlin/Higher-OrderFuncs.kt
@@ -0,0 +1,98 @@
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
+fun main() {
+    cookies.forEach {
+        println("Menu item: ${it.name}")
+    }
+
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+    println("\nFull menu:")
+    fullMenu.forEach {
+        println(it)
+    }
+
+    val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+    println("\nSoft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenuG = groupedMenu[true] ?: listOf()
+    val crunchyMenuG = groupedMenu[false] ?: listOf()
+    println("\nSoft cookies:")
+    softBakedMenuG.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenuG.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+    val totalPrice = cookies.fold(0.0) {total, cookie ->
+        total + cookie.price
+    }
+    println("\nTotal price: $${totalPrice}")
+
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+    println("\nAlphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
+}
